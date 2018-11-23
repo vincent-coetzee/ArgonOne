a378a762f7775a1dfc89138ed159929a226352f7
Vincent Coetzee <vincent.coetzee@glucode.com>
2018-11-24 01:55:39 +0200

    Replaced the VMThreadMemory based registers with registers in the thread itself. Hopefully should make the VM somewhat faster snce we are significantly reducing the number of cross language calls by replacing many of the C calls

---
+++ ArgonCompiler/Argon/Parser and Nodes/Support Types/Symbol.swift
@@ -0,0 +1,9 @@
+//
+//  Symbol.swift
+//  Argon
+//
+//  Created by Vincent Coetzee on 2018/11/23.
+//  Copyright © 2018 Vincent Coetzee. All rights reserved.
+//
+
+import Foundation
---
+++ ArgonCompiler/Argon/Virtual Machine/Object Pointers/HandlerBlockPointerWrapper.swift
@@ -0,0 +1,9 @@
+//
+//  HandlerBlockPointerWrapper.swift
+//  Argon
+//
+//  Created by Vincent Coetzee on 2018/11/23.
+//  Copyright © 2018 Vincent Coetzee. All rights reserved.
+//
+
+import Foundation
